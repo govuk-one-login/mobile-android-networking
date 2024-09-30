b2bd29b16773cb15b65910f24941051e0dec0eec
Bianca Mihaila <137913588+BiancaMihaila@users.noreply.github.com>
2024-09-30 09:32:02 +0100

    fix: Revert "Enable access token expred return from Auth web calls" (#84)
    - this reverts commit 5f8285d470be92766b662835522ac9bc6f598b51.

--- network/src/main/java/uk/gov/android/network/api/ApiFailureReason.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.android.network.api
-
-sealed class ApiFailureReason {
-    data object AccessTokenExpired : ApiFailureReason()
-    data object General : ApiFailureReason()
-    data object AuthProviderNotInitialised : ApiFailureReason()
-    data object Non200Response : ApiFailureReason()
-    data object AuthFailed : ApiFailureReason()
-}
