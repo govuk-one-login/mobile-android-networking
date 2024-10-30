09aeca7a93f328c2f6321c0dedd1bcd41564f2e5
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-10-30 19:18:22 +0000

    fix: Breaking Pipeline Change
    Update build.gradle.kts to include a property extra for buildLogicDir
    
    Resolves: 10478

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import uk.gov.pipelines.config.ApkConfig
 buildscript {
     val projectKey: String by rootProject.extra("mobile-android-networking")
     val projectId: String by rootProject.extra("uk.gov.android")
+    val buildLogicDir: String by rootProject.extra("mobile-android-pipelines/buildLogic")
 
     repositories {
         google()
