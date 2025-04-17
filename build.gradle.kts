3bc51e20f01c1f18571974dc7ec3c1cbfbed2b13
Bianca Mihaila <137913588+BiancaMihaila@users.noreply.github.com>
2025-04-17 09:46:50 +0100

    build: Update SdkVersions target and compile to API 35 (#206)
    * build: Update SdkVersions target and compile to API 35
    
    * ci: Upgrade pipelines mdoule to fix mergining issues
    
    Resolves: DCMAW-11313

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ val apkConfig by rootProject.extra(
         override val debugVersion: String = "DEBUG_VERSION"
         override val sdkVersions = object: ApkConfig.SdkVersions {
             override val minimum = 29
-            override val target = 33
-            override val compile = 34
+            override val target = 35
+            override val compile = 35
         }
     }
 )
